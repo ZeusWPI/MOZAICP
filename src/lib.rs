575e844bc0e9214be2b7b0e23ffaa70eeaaaddcb
ajuvercr <arthur.vercruysse@ugent.be>
2020-02-12 16:48:21 +0100

    update version + remove useless deps

--- src/lib.rs
+++ src/lib.rs
@@ -16,24 +16,12 @@ extern crate serde_json;
 extern crate tracing;
 extern crate tracing_futures;
 
-extern crate error_chain;
-
-extern crate capnp;
-extern crate capnp_futures;
-
 extern crate ws;
 
 #[macro_use]
 extern crate mozaic_derive;
 
-// pub mod messaging;
-// pub mod net;
-
-// pub mod runtime;
-
 pub mod modules;
-
-// pub mod errors;
 
 pub mod graph;
 pub mod generic;
